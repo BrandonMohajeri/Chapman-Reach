12fee1551bb5dfc59b304ef86eaac3a5fdf2c384
JRau <green302@mail.chapman.edu>
2015-07-08 20:41:18 -0700

    Changes to be committed: 	modified:   app/models/Student.rb 	modified:   app/views/home/home.html.erb 	modified:   app/views/layouts/application.html.erb

--- Reach/app/models/Student.rb
+++ Reach/app/models/Student.rb
@@ -6,24 +6,27 @@
 class Student 
 
 	attr_reader :studentUsername
+	attr_accessor :studentGender
+	attr_accessor :studentYear
 	attr_accessor :studentPassword
 	attr_accessor :studentEmail
 	attr_accessor :studentFirstName
 	attr_accessor :studentLastName
 	attr_accessor :studentMiddleInitial
-	attr_accessor :studentMajor
 
 
-	def initialize(user, pass, email, firstName, lastName, middleInitial, major = "undeclare")
+	def initialize(user, pass, email, firstName, lastName, middleInitial, gender, year, major = "undeclare")
  		@studentUsername = user
  		@studentPassword = pass
  		@studentEmail = email
  		@studentFirstName = firstName
  		@studentLastName = lastName
  		@studentMiddleInitial = middleInitial
+ 		@studentGender = gender
+ 		@studentYear = year
  		@studentMajor = new Major(major);
  		@studentCompleteClasses = Array.new;
- 		
+ 	
 	end
 
 
